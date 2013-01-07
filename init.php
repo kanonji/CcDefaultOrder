98b4b6fc44b1df7aa55aa06c4b213fdd3cbc4be0
kanonji <kanonji@localhost>
2013-01-07 15:13:20 +0900

    hardcoded default sort

--- init.php
+++ init.php
@@ -2,4 +2,15 @@
 $pluginContainer = ClassRegistry::getObject('PluginContainer');
 $pluginContainer->installed('cc_default_order','0.1');
 
+App::uses('CakeEventManager', 'Event');
 
+CakeEventManager::instance()->attach(function($event) {
+    if( 'issues' === $event->subject->request->controller && 'index' === $event->subject->request->action ){
+        $named = $event->subject->request->params['named'];
+        $sort = Set::get($named, 'sort');
+        $direction = Set::get($named, 'direction');
+        $named['sort'] = $sort ? $sort : 'Priority.position';
+        $named['direction'] = $direction ? $direction : 'desc';
+        $event->subject->request->params['named'] = $named;
+    }
+}, 'Controller.initialize');
